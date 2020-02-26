173c5195fc3b1f9f8504ed84c61e8d49cb1415ae
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-26 11:30:54 +0530

    MODIFIED :Store into dictionary and get the result

--- ArithmeticSorting.sh
+++ ArithmeticSorting.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -x
 echo "Welcome to  Sorting Arithmetic problem" 
-#TAKING THE THREE INPUTS  
+#TAKING THE THREE INPUTS
+declare -A Arithmetic  
 read -p "Enter the value of a :" a
 read -p "Enter the value of b :" b
 read -p "Enter the value of c :" c
-result=`echo "$a + $b * $c" | bc -l`
+result1=`echo "$a + $b * $c" | bc -l`
+Arithmetic[1]="$result1"
 #TAKING A NEW  EXP[RESSION AND FINDING A RESULT 
-result1=`echo "$a * $b + $c" | bc -l` 
-result2=`echo "$c + $a / $b" | bc -l`
-result3=`echo "$a % $b / $c" | bc -l` 
-echo " Computing the result : $result  "
-echo "Computing the result1 : $result1 " 
-echo "Computing the result2 : $result2 "
-echo "Computing the result3 : $result3 "
+result2=`echo "$a * $b + $c" | bc -l`
+Arithmetic[2]="$result2"
+result3=`echo "$c + $a / $b" | bc -l`
+Arithmetic[3]="$result3"
+result4=`echo "$a % $b / $c" | bc -l` 
+Arithmetic[4]="$result4"
+echo "Computing the result1 :"${!Arithmetic[@]}  : ${Arithmetic[@]}  "
+echo "Computing the result2 :"${!Arithmetic[@]}  : ${Arithmetic[@]} " 
+echo "Computing the result3 :"${!Arithmetic[@]}  : ${Arithmetic[@]} "
+echo "Computing the result4 :"${!Arithmetic[@]}  : ${Arithmetic[@]} "
