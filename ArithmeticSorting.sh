b380b6c547f89a9ec503b8a094ca4c3f8acf47a1
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-26 10:50:39 +0530

    MODIFIED :Added a new Expression

--- ArithmeticSorting.sh
+++ ArithmeticSorting.sh
@@ -7,7 +7,9 @@ read -p "Enter the value of c :" c
 result=`echo "$a + $b * $c" | bc -l`
 #TAKING A NEW  EXP[RESSION AND FINDING A RESULT 
 result1=`echo "$a * $b + $c" | bc -l` 
-result2=`echo "$c + $a / $b" | bc -l` 
-echo " Computing the result : $result "
+result2=`echo "$c + $a / $b" | bc -l`
+result3=`echo "$a % $b / $c" | bc -l` 
+echo " Computing the result : $result  "
 echo "Computing the result1 : $result1 " 
 echo "Computing the result2 : $result2 "
+echo "Computing the result3 : $result3 "
